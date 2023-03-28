15376c5f7c209014052513e733857e5a3fdc69e0
Alejandro <ajgarciabouzada@gmail.com>
2023-03-28 19:53:41 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+
+namespace EjercicioI03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero: ");
+            //int numeroIngresado = int.Parse(Console.ReadLine());
+            //falta validar el dato
+            bool rtaParse = int.TryParse(Console.ReadLine(), out int numeroIngresado);
+
+            if (rtaParse)
+            {
+                Console.WriteLine(ListarNumerosPrimos(numeroIngresado));
+            }
+            else
+            {
+                Console.WriteLine("No ingreso solo numeros.");
+            }
+
+
+            // agrego un comentario para ver el cambio en el GIT
+        }
+
+        public static string ListarNumerosPrimos(int numeroTope)
+        {
+            StringBuilder sb = new StringBuilder(); //nos permite concatenar strings
+
+            for(int i = 2; i <= numeroTope; i++)
+            {
+                if (EsPrimo(i))
+                {
+                    sb.AppendFormat("{0}, ", i);
+                }
+            }
+            return sb.ToString();
+        }
+
+        public static bool EsNumerico(string cadena)
+        {
+            return int.TryParse(cadena, out int numero);
+        }
+
+        public static bool EsPrimo(int numero)
+        {
+            for(int i = 2; i < numero; i++)
+            {
+                if(numero % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
